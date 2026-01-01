68b433e47b0b9c98abc5e93855782f21df5c332f
marangbabu <162810692+bijay-murmu@users.noreply.github.com>
2026-01-01 19:21:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { Telegraf } from "telegraf";
 import { exec } from "child_process";
 import fs from "fs";
 
-const bot = new Telegraf("PASTE_YOUR_BOT_TOKEN");
+const bot = new Telegraf("7653619302:AAFC5awOLf0tBK7RcxYAb_wjH00FD2oYv84");
 
 bot.start(ctx => {
   ctx.reply("👋 Send YouTube Shorts link");
